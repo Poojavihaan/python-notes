8ae274c98a1cf0653beeb76595d35c5795e99657
poojavihaan <uif69350@contiwan.com>
2025-02-22 19:39:06 +0530

    python course

---
+++ day1.py
@@ -0,0 +1,17 @@
+#variable
+
+
+# welcome1="*************"
+# welcome2="   welcome   "
+# welcome3="      to     "
+# welcome4="    python   "
+# welcome5="*************"
+
+
+# print(welcome1)
+# print(welcome2)
+# print(welcome3)
+# print(welcome4)
+# print(welcome5)
+
+
